e21a60a6569914eb5f47eda61f751af887581916
Cale Weaver <Jcaleweaver@gmail.com>
2020-03-19 20:18:31 +0100

    Change from xls to xlsx to reflect change from source data

--- covid-19 data.R
+++ covid-19 data.R
@@ -4,8 +4,8 @@ library(tidyverse)
 library(flexdashboard)
 library(DT)
 
-url1 <- paste0("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",Sys.Date(),".xls")
+url1 <- paste0("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",Sys.Date(),".xlsx")
 
-GET(url1, write_disk(tf <- tempfile(fileext = ".xls")))
+GET(url1, write_disk(tf <- tempfile(fileext = ".xlsx")))
 
 df <- read_excel((tf))
